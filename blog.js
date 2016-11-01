6a77acd1755b74f11f74c76a6eb939f930f661f4
precioustosin <precioustosin@hotmail.com>
2016-11-01 02:33:59 +0100

    installed express-handlebars

--- blog.js
+++ blog.js
@@ -6,6 +6,7 @@ var app = express();
 //setting localhost and port
 app.set('port', process.env.PORT || 8000);
 
+//middleware to access static
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
