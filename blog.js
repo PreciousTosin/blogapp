33a0d613120a3beb7b5d6fe82bf7a59666506dd8
precioustosin <precioustosin@hotmail.com>
2016-11-01 01:37:25 +0100

    added test route, custom error routes: 404 and 500

--- blog.js
+++ blog.js
@@ -5,6 +5,23 @@ var app = express();
 //setting localhost and port
 app.set('port', process.env.PORT || 8000);
 
+app.get('/', function(req, res){
+	res.json("WELCOME TO BLOG EXPRESS APPLICATION")
+});
+
+// custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost: ' + app.get('port') + ' press Ctr-c to terminate');
